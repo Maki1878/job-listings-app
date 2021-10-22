f043b2d9527a4ba96b6c9f04c1b0c7d49108f6fe
Marko Markotić <markomarkotic05@gmail.com>
2021-10-22 07:44:07 +0200

    added functionality for dinamically adding the header image depending on the screen size

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,38 @@
 <template>
-  <img :src="imgPath" class="header" />
+  <img :src="setHeader" class="header" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      imgPath: require('@/assets/images/background/bg-header-desktop.svg'),
+      isMobile: false,
     };
+  },
+  beforeMount() {
+    window.addEventListener('resize', this.boundOnResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.boundOnResize);
+  },
+  computed: {
+    setHeader() {
+      return this.isMobile
+        ? require('@/assets/images/background/bg-header-mobile.svg')
+        : require('@/assets/images/background/bg-header-desktop.svg');
+    },
+    boundOnResize() {
+      return this.onResize.bind(this);
+    },
+  },
+  methods: {
+    onResize() {
+      if (window.innerWidth <= 375) {
+        this.isMobile = true;
+      } else {
+        this.isMobile = false;
+      }
+    },
   },
 };
 </script>
@@ -15,7 +40,7 @@ export default {
 <style scoped>
 .header {
   background-color: var(--color-primary);
-  width: 144rem;
+  width: 100%;
   height: 11rem;
   object-fit: cover;
   margin-bottom: 2rem;
